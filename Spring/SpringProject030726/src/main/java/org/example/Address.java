3ed160a5305e8fe395ac1849820f718f29c055ce
vishnuasdev <vishnuveerappan21@gmail.com>
2026-07-07 10:53:57 +0530

    hd

--- Spring/SpringProject030726/src/main/java/org/example/Address.java
+++ Spring/SpringProject030726/src/main/java/org/example/Address.java
@@ -11,4 +11,23 @@ public class Address {
     public String getCity(){
         return city;
     }
+
+
+//    public Address(String city){
+//        this.city = city;
+//    }
+//
+//
+//    public Address(String city, int pincode){
+//        this.city = city;
+//        this.pincode = pincode;
+//    }
+//
+//    public String getCity(){
+//        return city;
+//    }
+//
+//    public String getPincode(){
+//        return pincode;
+//    }
 }
